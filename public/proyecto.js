e4647d66106c5a2d7e37b21a0a125963ef0bc075
floyd-94 <s.saldana.pimentel@gmail.com>
2023-08-25 15:12:32 -0300

    agregué menú y responsive del mismo hasta 800px

--- public/proyecto.js
+++ public/proyecto.js
@@ -1,20 +1,33 @@
 
-var botonAbrirDesplegable = document.querySelector(".boton-abrir-desplegable");
-var botonCerrarDesplegable = document.querySelector(".boton-cerrar-desplegable");
-var contenido = document.querySelector(".contenido-colapsable");
+var botonesAbrirDesplegable = document.querySelectorAll(".boton-abrir-desplegable");
+var botonesCerrarDesplegable = document.querySelectorAll(".boton-cerrar-desplegable");
+var contenidos = document.querySelectorAll(".contenido-colapsable");
 
 
-function abrirContenido() {
-    contenido.classList.add("desplegar");
-    botonCerrarDesplegable.style.display = "block";
-    botonAbrirDesplegable.style.display = "none";
+function abrirContenido(i) {
+    contenidos[i].classList.add("desplegar");
+    botonesCerrarDesplegable[i].style.display = "block";
+    botonesAbrirDesplegable[i].style.display = "none";
 }
 
-function cerrarContenido() {
-    contenido.classList.remove("desplegar");
-    botonCerrarDesplegable.style.display = "none";
-    botonAbrirDesplegable.style.display = "block";
+function cerrarContenido(i) {
+    contenidos[i].classList.remove("desplegar");
+    botonesCerrarDesplegable[i].style.display = "none";
+    botonesAbrirDesplegable[i].style.display = "block";
 }
 
-botonAbrirDesplegable.addEventListener("click", abrirContenido);
-botonCerrarDesplegable.addEventListener("click", cerrarContenido);
+
+for (let i = 0; i < botonesAbrirDesplegable.length; i++) {
+    const botonAbrir = botonesAbrirDesplegable[i];
+    const botonCerrar = botonesCerrarDesplegable[i];
+
+    botonAbrir.addEventListener("click", function(){
+        abrirContenido(i)
+    })
+    
+    botonCerrar.addEventListener("click", function(){
+        cerrarContenido(i)
+    });
+} 
+
+console.log("hola")
